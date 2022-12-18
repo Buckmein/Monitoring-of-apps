5e9c42775b666ce5af3620b9f23d75351a72ea2c
Buckmein <dodin.dima@gmail.com>
2022-12-18 22:31:45 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace _2_курс_мониторинг_частоты_использованяи_приложения
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dg2 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sb = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg1
+            // 
+            this.dg1.AllowUserToAddRows = false;
+            this.dg1.AllowUserToDeleteRows = false;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dg1.Location = new System.Drawing.Point(12, 12);
+            this.dg1.Name = "dg1";
+            this.dg1.ReadOnly = true;
+            this.dg1.Size = new System.Drawing.Size(411, 426);
+            this.dg1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Имя процесса";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 96;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите процессы (Не более трех)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(432, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 47);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // dg2
+            // 
+            this.dg2.AllowUserToAddRows = false;
+            this.dg2.AllowUserToDeleteRows = false;
+            this.dg2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3});
+            this.dg2.Location = new System.Drawing.Point(463, 150);
+            this.dg2.Name = "dg2";
+            this.dg2.ReadOnly = true;
+            this.dg2.Size = new System.Drawing.Size(245, 103);
+            this.dg2.TabIndex = 5;
+            this.dg2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView2_CellContentClick);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Имя процесса";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Количество открытий ";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // sb
+            // 
+            this.sb.Location = new System.Drawing.Point(633, 259);
+            this.sb.Name = "sb";
+            this.sb.Size = new System.Drawing.Size(75, 23);
+            this.sb.TabIndex = 6;
+            this.sb.Text = "Сброс";
+            this.sb.UseVisualStyleBackColor = true;
+            this.sb.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(463, 259);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Начать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(515, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Сохранить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 473);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.sb);
+            this.Controls.Add(this.dg2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dg1);
+            this.Name = "Form1";
+            this.Text = "Мониторинг частоты использования приложений";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dg2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button sb;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Management;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SProcess();
+        }
+        string[] nm = new string[100];
+        int[] cnt = new int[100];
+
+        void SProcess()
+        {
+
+            ManagementClass proc = new ManagementClass("Win32_Process");
+            ManagementObjectCollection a = proc.GetInstances();
+            foreach (ManagementObject d1 in a)
+            {
+                dg1.Rows.Add(d1["Name"]);
+                label1.Text = Convert.ToString(d1["Handle"]);
+            }
+        }
+        static public int j;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    cnt[i] = -33;
+                }
+                string a;
+                StreamReader f = new StreamReader("cfg.txt");
+                string[] str;
+                j = 0;
+                while ((f.ReadLine()) != null)
+                {
+                    a = f.ReadLine();
+                    str = a.Split(new char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    nm[j] = str[0];
+                    cnt[j] = Convert.ToInt32(str[1]);
+                    j++;
+                }
+                f.Close();
+            }
+            catch
+            {
+                label1.Text = "Ошибка файла";
+            }
+        }
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            dg1.Rows.Clear();
+            SProcess();
+        }
+        int[] d = { 1, 1, 1 };
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            bool z = true;
+            if (dg2.Rows.Count != 3)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (Convert.ToString(dg1.CurrentCell.Value) == nm[i])
+                    {
+                        dg2.Rows.Add(nm[i], cnt[i]);
+                        z = false;
+                    }
+                }
+                if (z)
+                {
+                    dg2.Rows.Add(dg1.CurrentCell.Value, 1);
+                }
+                dg1.Rows.Remove(dg1.CurrentRow);
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (dg2.Rows.Count != 0)
+            {
+                dg1.Rows.Add(dg2.CurrentCell.Value);
+                dg2.Rows.Remove(dg2.CurrentRow);
+            }
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                d[i] = 0;
+            }
+            for (int i = 0; i < dg2.RowCount; i++)
+                dg2[1, i].Value = Convert.ToString(d[0]);
+        }
+        private void DataGridView2_CellContentClick(object sender, EventArgs e)
+        {
+
+        }
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ManagementEventWatcher startWatch = new ManagementEventWatcher(
+                   new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+                startWatch.EventArrived += StartWatch_EventArrived;
+                startWatch.Start();
+            }
+            catch { }
+        }
+        private void Button4_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                bool z = true;
+                for (int y = 0; y < 3; y++)
+                {
+                    z = true;
+                    for (int i = 0; i < 100; i++)
+                    {
+                        if ((Convert.ToString(dg2[0, y].Value) == nm[i]) & (z))
+                        {
+                            nm[i] = Convert.ToString(dg2[0, y].Value);
+                            cnt[i] = d[y];
+                            z = false;
+                        }
+                    }
+                    if (z)
+                    {
+                        cnt[j] = d[y];
+                        nm[j] = Convert.ToString(dg2[0, y].Value);
+                        j++;
+                    }
+                }
+                StreamWriter sr = new StreamWriter("cfg.txt", false);
+                sr.WriteLine("Название_Процесса Количество");
+                for (int i = 0; i < 100; i++)
+                {
+                    if (cnt[i] != -33)
+                    {
+                        sr.WriteLine(nm[i] + " " + cnt[i]);
+                    }
+                }
+                sr.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        void StartWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (Convert.ToString(e.NewEvent.Properties["ProcessName"].Value) == Convert.ToString(dg2[0, i].Value))
+                {
+                    d[i] = Convert.ToInt32(dg2[1, i].Value);
+                    d[i]++;
+                    dg2[1, i].Value = Convert.ToString(d[i]);
+                }
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+    
